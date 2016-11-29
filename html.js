73a79d2c2f6ce7ae0785d370f72c18689816306d
andresattler <andresattler.webdev@gmail.com>
2016-11-29 09:35:23 -0400

    changed relative stylesheetpath to absolute

--- html.js
+++ html.js
@@ -7,10 +7,10 @@ class Html extends React.Component {
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          <link rel="stylesheet" href="./css/style.css"/>
+          <link rel="stylesheet" href="/css/style.css"/>
         </head>
         <body>
-          <Template content={this.props.content}/>
+          <Template content={this.props}/>
         </body>
       </html>
     )
